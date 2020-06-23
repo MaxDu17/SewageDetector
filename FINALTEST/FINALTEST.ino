22525267f4eb7adcd35a8bc90b9ca64ccd3c1f9e
Maximilian Du <32785271+MaxDu17@users.noreply.github.com>
2020-06-23 15:42:57 -0400

    Create FINALTEST.ino

---
+++ FINALTEST/FINALTEST.ino
@@ -0,0 +1,46 @@
+#define THRESHOLD 73
+void setup() {
+ pinMode(13, OUTPUT);
+ pinMode(12, OUTPUT);
+ pinMode(8, INPUT);
+ Serial.begin(9600);
+ digitalWrite(13, LOW);
+}
+
+int strikes = 0; 
+void loop() {
+  long delay_avg = 0;
+  for(int i = 0; i < 1000; i ++)
+  {
+    long delay_ = pulseIn(8, HIGH);
+    delay_avg = delay_avg + delay_;
+  }
+
+  delay_avg = delay_avg/1000;
+  
+  if(delay_avg >THRESHOLD)
+  {
+    strikes ++; 
+    digitalWrite(12, HIGH);
+  }
+  else
+  {
+    digitalWrite(12, LOW);
+    strikes --;
+  }
+  
+  if(strikes > 40)
+  {
+    digitalWrite(13, HIGH);
+    while(true)
+    {
+      digitalWrite(12, HIGH);
+      delay(500);
+      digitalWrite(12, LOW);
+      delay(500);
+    }
+  }
+  
+  delay_avg = 0;
+
+}
